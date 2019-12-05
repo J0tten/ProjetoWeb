b914867ac8d3a0144d40fc2da2a9fe1ee11179ff
GUILHERME <GUILHERME@GUILHERME-PC>
2019-12-04 23:17:18 -0200

    DAO produto

--- VeganWash/src/java/br/edu/ufabc/VeganWash/dao/ProdutoDAO.java
+++ VeganWash/src/java/br/edu/ufabc/VeganWash/dao/ProdutoDAO.java
@@ -5,6 +5,11 @@
  */
 package br.edu.ufabc.VeganWash.dao;
 
+import br.edu.ufabc.VeganWash.model.Fornecedor;
+import br.edu.ufabc.VeganWash.model.Limpeza;
+import br.edu.ufabc.VeganWash.model.Produto;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.util.List;
 
 /**
@@ -13,9 +18,31 @@ import java.util.List;
  */
 public class ProdutoDAO implements GenericDAO {
 
+    private final DataSource dataSource;
+
+    public ProdutoDAO(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Override
     public void create(Object o) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Produto c = (Produto) o;
+        Fornecedor f = (Fornecedor) o;
+        Limpeza z = (Limpeza) o;
+        try {
+            String SQL = "INSERT INTO produto values (null, ?, ?, ?)";
+            PreparedStatement stm = dataSource.getConnection().prepareStatement(SQL, Statement.RETURN_GENERATED_KEYS);
+            stm.setInt(1, f.getIdFornecedor());
+            stm.setInt(2, z.getIdLimpeza());
+            stm.setDouble(3, c.getValorM2());
+            int result = stm.executeUpdate();
+            if (result == 0) {
+                throw new RuntimeException("Falha ao inserir - registros duplicados");
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
     }
 
     @Override
@@ -32,5 +59,5 @@ public class ProdutoDAO implements GenericDAO {
     public List<Object> read() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
+
 }
