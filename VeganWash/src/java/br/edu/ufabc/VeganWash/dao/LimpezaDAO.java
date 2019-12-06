62f419a8aec1feec86eb99b015c10799cae8619a
GUILHERME <GUILHERME@GUILHERME-PC>
2019-12-06 21:13:01 -0200

    limpeza dao

---
+++ VeganWash/src/java/br/edu/ufabc/VeganWash/dao/LimpezaDAO.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ufabc.VeganWash.dao;
+
+import br.edu.ufabc.VeganWash.model.Limpeza;
+import br.edu.ufabc.VeganWash.model.Usuario;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author GUILHERME
+ */
+public class LimpezaDAO {
+
+    private final DataSource dataSource;
+
+    public LimpezaDAO(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public void create(Object o) {
+    }
+
+    public void delete(Object o) {
+    }
+
+    public void update(Object o) {
+    }
+
+    public List<Object> read() {
+         List<Object> result = null;
+        try {
+            String sql = "SELECT * FROM limpeza";
+            PreparedStatement stmread = dataSource.getConnection().prepareStatement(sql);
+            ResultSet rs = stmread.executeQuery();
+            result = new ArrayList<>();
+            while (rs.next()) {
+                Limpeza l = new Limpeza();
+                l.setIdLimpeza(rs.getInt("idLimpeza")); // aqui é o nome da coluna na tablea
+                l.setNomeLimpeza(rs.getString("nomeLimpeza"));
+                l.setDescLimpeza(rs.getString("descLimpeza"));
+                result.add(l);
+            }
+            rs.close();
+            stmread.close();
+        } catch (Exception ex) {
+            System.out.println("Limpeza.READ - erro ao recuperar");
+            System.out.println(ex.getMessage());
+        }
+        return result;
+    }
+}
